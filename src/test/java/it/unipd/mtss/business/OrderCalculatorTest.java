ce19e50b020eae449074b8b60793a601228a0d73
alecsferra <alecsferra@protonmail.com>
2022-05-21 16:48:24 +0200

    Implementato regalo per utenti minorenni
    + Diminuito delta di errore
    
    Close #14

--- src/test/java/it/unipd/mtss/business/OrderCalculatorTest.java
+++ src/test/java/it/unipd/mtss/business/OrderCalculatorTest.java
@@ -4,29 +4,64 @@ import it.unipd.mtss.business.exceptions.OrderBillException;
 import it.unipd.mtss.model.EItem;
 import it.unipd.mtss.model.ItemType;
 import it.unipd.mtss.model.User;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class OrderCalculatorTest {
 
-  private static final double DELTA = 0.09;
-  private static OrderCalculator order;
+  private static final double DELTA = 0.0009;
+  private static final LocalTime IN_TIME = LocalTime.of(18, 30);
+  private static final LocalTime BEFORE_TIME = LocalTime.of(2, 0);
+  private static final LocalTime AFTER_TIME = LocalTime.of(20, 0);
 
-  private static User user;
+  private OrderCalculator alwaysFalseInTime;
+  private OrderCalculator alwaysTrueInTime;
+  private OrderCalculator alwaysFalseBeforeTime;
+  private OrderCalculator alwaysFalseAfterTime;
+  private OrderCalculator alwaysTrueBeforeTime;
+  private OrderCalculator alwaysTrueAfterTime;
+
+  private static User adult;
+  private static User minor;
+
+  private static Random alwaysFalse;
+  private static Random alwaysTrue;
 
   @BeforeClass
   public static void classSetUp() {
-    order = new OrderCalculator();
-    user = new User("User", 19);
+    alwaysTrue = mock(Random.class);
+    when(alwaysTrue.nextBoolean()).thenReturn(true);
+
+    alwaysFalse = mock(Random.class);
+    when(alwaysFalse.nextBoolean()).thenReturn(false);
+
+    adult = new User("Adulto", 19);
+    minor = new User("Bocia", 1);
+  }
+
+  @Before
+  public void setUp() {
+    alwaysFalseInTime = new OrderCalculator(alwaysFalse, IN_TIME);
+    alwaysTrueInTime = new OrderCalculator(alwaysTrue, IN_TIME);
+
+    alwaysFalseAfterTime = new OrderCalculator(alwaysFalse, AFTER_TIME);
+    alwaysFalseBeforeTime = new OrderCalculator(alwaysFalse, BEFORE_TIME);
+
+    alwaysTrueAfterTime = new OrderCalculator(alwaysTrue, AFTER_TIME);
+    alwaysTrueBeforeTime = new OrderCalculator(alwaysTrue, BEFORE_TIME);
   }
 
   @Test
@@ -34,7 +69,7 @@ public class OrderCalculatorTest {
 
     NullPointerException exc = assertThrows(
         NullPointerException.class,
-        () -> order.getOrderPrice(null, user)
+        () -> alwaysFalseInTime.getOrderPrice(null, adult)
     );
 
     assertEquals("The ordered items must not be null", exc.getMessage());
@@ -51,7 +86,7 @@ public class OrderCalculatorTest {
 
     NullPointerException exc = assertThrows(
         NullPointerException.class,
-        () -> order.getOrderPrice(products, null)
+        () -> alwaysFalseInTime.getOrderPrice(products, null)
     );
 
     assertEquals("The user must not be null", exc.getMessage());
@@ -67,7 +102,7 @@ public class OrderCalculatorTest {
 
     OrderBillException exc = assertThrows(
         OrderBillException.class,
-        () -> order.getOrderPrice(products, user)
+        () -> alwaysFalseInTime.getOrderPrice(products, adult)
     );
 
     assertEquals(
@@ -83,7 +118,7 @@ public class OrderCalculatorTest {
 
     OrderBillException exc = assertThrows(
         OrderBillException.class,
-        () -> order.getOrderPrice(products, user)
+        () -> alwaysFalseInTime.getOrderPrice(products, adult)
     );
 
     assertEquals(
@@ -99,7 +134,7 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Motherboard, "Prodotto positivo", 0)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(2, computedPrice, DELTA);
   }
@@ -112,7 +147,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Motherboard, "MadreTavola", 31)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(100.0, computedPrice, DELTA);
   }
@@ -124,7 +159,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Mouse, "MegaMickey", 22)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(22.0, computedPrice, DELTA);
   }
@@ -142,7 +177,7 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Processor, "ProcessoreCostoMinimo", 30)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(315.0, computedPrice, DELTA);
   }
@@ -164,7 +199,7 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Mouse, "Mouse10", 10)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(100.0, computedPrice, DELTA);
   }
@@ -178,7 +213,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Motherboard, "ProdottoMenoCaro", 20)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(100.0, computedPrice, DELTA);
   }
@@ -198,7 +233,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Processor, "ProcessoreCostoMinimo", 30)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(365, computedPrice, DELTA);
   }
@@ -211,7 +246,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Processor, "Processore costoso", 550)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(990, computedPrice, DELTA);
   }
@@ -232,7 +267,7 @@ public class OrderCalculatorTest {
             new EItem(ItemType.Motherboard, "MOBOCostosa", 800)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(1048.5, computedPrice, DELTA);
   }
@@ -246,7 +281,7 @@ public class OrderCalculatorTest {
 
     OrderBillException exc = assertThrows(
         OrderBillException.class,
-        () -> order.getOrderPrice(products, user)
+        () -> alwaysFalseInTime.getOrderPrice(products, adult)
     );
 
     assertEquals(
@@ -261,7 +296,7 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Keyboard, "Tastierina", 9)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(11, computedPrice, DELTA);
   }
@@ -279,7 +314,7 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Processor, "Processore5", 3.5)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(11.8, computedPrice, DELTA);
   }
@@ -301,8 +336,161 @@ public class OrderCalculatorTest {
         new EItem(ItemType.Mouse, "Mouse10", 0.99)
     );
 
-    var computedPrice = order.getOrderPrice(products, user);
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
 
     assertEquals(11.9, computedPrice, DELTA);
   }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueInTime.getOrderPrice(products, minor);
+
+    assertEquals(0, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, minor);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndAfterWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueAfterTime.getOrderPrice(products, minor);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndBeforeWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueBeforeTime.getOrderPrice(products, minor);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndAfterWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseAfterTime.getOrderPrice(products, minor);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnMinorUserAndBeforeWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseBeforeTime.getOrderPrice(products, minor);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueInTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseInTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndAfterWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueAfterTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndBeforeWinningTimeAndLucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysTrueBeforeTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndAfterWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseAfterTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+
+  @Test
+  public void testGetOrderPrice_OnAdultUserAndBeforeWinningTimeAndUnlucky()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+    var computedPrice = alwaysFalseBeforeTime.getOrderPrice(products, adult);
+
+    assertEquals(13, computedPrice, DELTA);
+  }
+  @Test
+  public void
+  testGetOrderPrice_OnMinorUserAndWinningTimeAndLuckyButEleventhGift()
+          throws OrderBillException {
+    var products = List.of(
+            new EItem(ItemType.Processor, "Provola", 13)
+    );
+
+    List<Double> prices = new ArrayList<>();
+
+    for(int i = 0; i < 11; ++i) {
+      prices.add(alwaysTrueInTime.getOrderPrice(products, minor));
+    }
+
+    var expectedPrice = List.of(
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 13.0
+    );
+    assertEquals(expectedPrice, prices);
+  }
+
 }
